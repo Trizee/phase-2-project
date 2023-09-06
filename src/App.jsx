361f86f0e5e6a03e2b0c059471bc1456fce134ae
Tri Hoang <trihoang09@gmail.com>
2023-09-06 10:51:08 -0600

    Revert "messed something up"
    This reverts commit 2d99422c3b340af5fcbe65deffcbf367c2eca6f2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RootLayout from './layout/RootLayout';
 import Landing from './components/Landing';
@@ -20,7 +20,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Landing />} />
       <Route path='/home' element={<Landing />}/>
-      <Route path='/shop' element={<Shop/>}/>
+      <Route path='/shop' element={<Shop />}/>
       <Route path='/cart' element={<Cart />}/>
       <Route path='/reviews' element={<Reviews />}/>
     </Route>
