fa02fb0117bdec97fae75f9c05ad4ccdd149fca5
Tri Hoang <trihoang09@gmail.com>
2023-09-07 13:21:25 -0600

    made skeleton for patch function

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,18 +1,35 @@
 import Card from 'react-bootstrap/Card';
 import { Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from 'react';
 
 function ReviewCard({data, deleteReview}){
 
     const {name , review} = data
+
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     
 
     return (
+      <>
         <Card style={{padding: '10px' , margin: '10px'}}>
           <Card.Header style={{textAlign: 'center'}}>Review
           <Button variant="text" color="error" style={{float: 'right'}} onClick={()=> deleteReview(data)}>
           🗑️
           </Button> 
-          <Button variant="text" color="error" style={{float: 'right'}}>
+          <Button variant="text" color="error" style={{float: 'right'}} onClick={handleClickOpen}>
           📝
           </Button> 
           
@@ -28,6 +45,29 @@ function ReviewCard({data, deleteReview}){
             </blockquote>
           </Card.Body>
         </Card>
+         <Dialog open={open} onClose={handleClose}>
+         <DialogTitle>Edit</DialogTitle>
+         <DialogContent>
+           <DialogContentText>
+             Make Changes
+           </DialogContentText>
+            <TextField
+             autoFocus
+             margin="dense"
+             id="review"
+             label="Edit Review"
+             type="text"
+             fullWidth
+             variant="standard"
+           />
+           
+         </DialogContent>
+         
+         <DialogActions>
+           <Button>Submit</Button>
+         </DialogActions>
+       </Dialog>
+      </>
       );
 }
 
